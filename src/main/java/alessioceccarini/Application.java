a1eeada90a78484217f1e5a3447e18410b709479
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-01-19 17:31:52 +0100

    adding_ex_with_new_pgadmin

--- src/main/java/alessioceccarini/Application.java
+++ src/main/java/alessioceccarini/Application.java
@@ -17,3 +17,13 @@
 //TOTALE IMPORTO FATTURE E SOMMA:
 //
 //--SELECT count(*),sum(import) FROM public.invoices
+//
+//
+//TOTALE FATTURE CON IVA 20:
+//
+//select count(*) from invoices where iva_import =20
+//
+//
+//TOTALE FATTURE 2017:
+//
+//select count(*) from invoices where iva_import =20 and EXTRACT( YEAR FROM date)=2017
